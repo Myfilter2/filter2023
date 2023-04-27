30c568bcc1e9b71c30f4bad08939b7dc6b6da39f
BlackList <119548568+Myfilter2@users.noreply.github.com>
2023-04-27 11:18:28 +0300

    Update pen_2023.js

--- pen_2023.js
+++ pen_2023.js
@@ -1,4 +1,4 @@
-/// pen2023.js
+/// pen_2023.js
 setTimeout(function (event){ fs.get('/contest/ajax/closePacks2022ContestBanner/', {}, function () { module.remove({click: this});});}, 1000);
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 addCSS(".moneybox-advert{background: url('https://i.redd.it/86aej5ehyxh11.gif') no-repeat center!important;}");
