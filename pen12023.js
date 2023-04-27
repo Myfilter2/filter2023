6d6585a721cb90e307d94144d94bc2d008be957f
BlackList <119548568+Myfilter2@users.noreply.github.com>
2023-04-27 11:25:36 +0300

    Update pen12023.js

--- pen12023.js
+++ pen12023.js
@@ -1,3 +1,5 @@
 /// pen12023.js
+function() {
+ 'use strict';
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
-addCSS(".moneybox-advert{background: url('https://i.redd.it/86aej5ehyxh11.gif') no-repeat center!important;}");
+addCSS(".moneybox-advert{background: url('https://i.redd.it/86aej5ehyxh11.gif') no-repeat center!important;}");}
